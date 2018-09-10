ad30af8cb0890f2374e8cb69894b00cb7f0f4967
aukustik <aukustik@yandex.ru>
2018-09-10 20:24:41 +0700

    kek
    keked

--- character.py
+++ character.py
@@ -3,15 +3,15 @@ class MainChar:
         self.health = 100
         self.name = nameChar
         self.exp = 0
-    
+        self.backpack = Storage(20)
+
     def take_damage(self, damage):
         self.health = self.health - damage
-        print(self.name)
-        print(self.health)
+        print(self.name, 'lost', damage, 'hp')
 
     def healing(self, heal):
         self.health = self.health + heal
-        print(self.health)
+        print(self.name,'healed',heal, 'hp')
 
 
 class Item:
@@ -20,6 +20,9 @@ class Item:
         self.itemSize = size
         self.quantity = 1
 
+class HealingPotion(Item):
+    def using(self, char):
+        char.healing(20)
 
 class Storage:
     def __init__(self, maxSize):
@@ -27,12 +30,14 @@ class Storage:
         self.storage = []
     
     def add_item(self, item):
-        if (item in self.storage):
-            self.storage.remove(item)
-            item.quantity = item.quantity + 1
-            self.storage.append(item)
+        if(len(self.storage) + item.itemSize > self.size):
+            print('not enough space')
         else:
             self.storage.append(item)
-
+        
     def remove_item(self, item):
+        self.storage.remove(item)
+
+    def use_item(self, item):
+        print('Using item', item.itemId)
         self.storage.remove(item)
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from character import MainChar
+from character import *
 
-kek = MainChar("lol")
-kek.take_damage(10)
-kek.healing(232)
+mainCharacter = MainChar("Vitalik")
+print(mainCharacter.name,'\'s hp = ',mainCharacter.health)
+potion = HealingPotion('Healing Potion', 1)
+mainCharacter.backpack.add_item(potion)
+mainCharacter.backpack.add_item(potion)
+royal = Item('Piano', 20)
+mainCharacter.backpack.add_item(royal)
+mainCharacter.take_damage(50)
+potion.using(mainCharacter)
+print(mainCharacter.name,'\'s hp =',mainCharacter.health)
