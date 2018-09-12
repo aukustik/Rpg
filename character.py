f0e7097cd676b24d30e50a02e2aa4005c78e4be1
aukustik <aukustik@yandex.ru>
2018-09-12 16:54:32 +0700

    kek
    Added equipment and helmets

--- character.py
+++ character.py
@@ -1,9 +1,21 @@
 class MainChar:
     def __init__(self, nameChar):
         self.health = 100
+        self.max_health = 100
         self.name = nameChar
         self.exp = 0
         self.backpack = Storage(20)
+        self.equipment = {
+            'Helmet':None,
+            'Body':None,
+            'Boots':None,
+            'Gloves':None,
+            'Weapon':None
+        }
+    def equip_item_helm(self, item):
+        if (item.type == 'Helmet'):
+            self.max_health += item.bonus_health
+            self.equipment['Helmet'] = item.itemId
 
     def take_damage(self, damage):
         self.health = self.health - damage
@@ -13,6 +25,10 @@ class MainChar:
         self.health = self.health + heal
         print(self.name,'healed',heal, 'hp')
 
+    def print_stats(self):
+        print('HERO STATS')
+        print('Heath Points:', self.health, '/',self.max_health)
+
 
 class Item:
     def __init__(self, name, size):
@@ -21,8 +37,14 @@ class Item:
         self.quantity = 1
 
 class HealingPotion(Item):
+    type = 'Potion'
     def using(self, char):
         char.healing(20)
+
+class Helmets(Item):
+    type = 'Helmet'
+    bonus_health = 20
+
 
 class Storage:
     def __init__(self, maxSize):
